f71eb911c64382e6337556b31a1b3ded0a842482
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-09-03 23:34:05 +1000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.get("/api/getEmails", (req, res) => {
+  const email = {
+    name: "Tsubasa",
+    email: "example@gmail.com",
+  };
+
+  return res.status(200).json(email);
+});
+
+app.listen(3000, () => {
+  console.log("App listening on port 3000");
+});
