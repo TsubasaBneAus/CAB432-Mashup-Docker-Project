c287c8b157962b3473f7999c2b870c26481a501a
Tsubasa Endo <tsubasa.bneaus@gmail.com>
2023-09-04 19:27:20 +1000

    create API for processing weather API

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,20 +1,60 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 
 const app = express();
+const PORT = 5000;
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true,
+  optionsSuccessStatus: 200,
+};
 
-app.use(cors());
+dotenv.config({
+  path: "../.env",
+});
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const NEWS_API_KEY = process.env.NEWS_API_KEY;
+const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY;
 
-app.get("/", (req, res) => {
-  const email = {
-    name: "Tsubasa",
-    email: "example@gmail.com",
-  };
+app.use(express.json());
+app.use(cors(corsOptions));
 
-  return res.status(200).json(email);
-})
+app.post("/getCurrentData", async (req, res) => {
 
+  // Fetch the current weather and forecast
+  const latitude = req.body.latitude;
+  const longitude = req.body.longitude;
+  const currentWeatherRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+  )
+  const weatherForecastRes = await fetch(
+    `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`
+  );
+  const currentWeatherData = await currentWeatherRes.json();
+  const weatherForecastData = await weatherForecastRes.json();
 
-app.listen(5000, () => {
+  // Process forecast data to display 24 hours of data from the time fetched
+  const currentDate = new Date();
+  let forecastArray = [];
+  for (let eachForecast of weatherForecastData.list) {
+    const forecastDate = new Date(eachForecast.dt_text);
+    
+    // Add forecast data to the array if the time of the forecast is after the time fetched
+    if (currentDate < forecastDate) {
+      forecastArray.push(eachForecast);
+      count++;
+    }
+
+    // Break if the array has more than 8 forecast data
+    if (forecastArray.length >= 8) {
+      break;
+    }
+  }
+  // console.log(json);
+  // return res.send(JSON.stringify(json.city));
+});
+
+app.listen(PORT, () => {
   console.log("App listening on port 5000");
 });
--- frontend/src/app/page.jsx
+++ frontend/src/app/page.jsx
@@ -3,26 +3,82 @@
 import { useState, useEffect } from "react";
 
 const Home = () => {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const res = await fetch("http://localhost:5000/");
-        const json = await res.json();
-        setData(json);
-      } catch (err) {
-        console.log(err);
-      }
-    };
+  const [currentData, setCurrentData] = useState({});
+  const [location, setLocation] = useState({
+    latitude: "",
+    longitude: "",
+  });
 
-    fetchData();
+  const [date, setDate] = useState();
+
+  // Fetch the current data on weather and news
+  const fetchCurrentData = async (latitude, longitude) => {
+    try {
+      const res = await fetch("http://localhost:5000/getCurrentData", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          latitude: latitude,
+          longitude: longitude,
+        }),
+      });
+      const data = await res.json();
+      console.log(data);
+      setCurrentData(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  // Fetch user's current location data
+  const fetchLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      setLocation({
+        latitude: latitude,
+        longitude: longitude,
+      });
+      fetchCurrentData(latitude, longitude);
+    });
+  };
+
+  const setdate = () => {
+    const dateObject = new Date();
+    const year = dateObject.getFullYear();
+    const month = ("0" + (dateObject.getMonth() + 1)).slice(-2);
+    const date = ("0" + dateObject.getDate()).slice(-2);
+    const hours = ("0" + dateObject.getHours()).slice(-2);
+    const minutes = ("0" + dateObject.getMinutes()).slice(-2);
+    const seconds = ("0" + dateObject.getSeconds()).slice(-2);
+    const currentDate = `${year}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+    console.log(currentDate);
+    console.log(new Date());
+    const date1 = new Date().getTime();
+    const date2 = new Date("2023-09-04 19:00:00").getTime();
+    if (date1 < date2) {
+      setDate("TRUE");
+    } else {
+      setDate("FALSE");
+    }
+  };
+
+  useEffect(() => {
+    // fetchLocation();
+    setdate();
   }, []);
 
   return (
     <div>
-      <h1>UserName: {data.name}, Email: {data.email}</h1>
+      {/* <h1 className="text-2xl">Fetched Info: {data.name} {data.country}</h1> */}
+      <h1 className="text-2xl">
+        {/* The current location: ({location.latitude}, {location.longitude}) */}
+        {date}
+      </h1>
     </div>
-  )
+  );
 };
 
 export default Home;
